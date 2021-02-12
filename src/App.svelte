e0a3fe12764d515f3903c0a2d4642b95f370df05
clpi <clp@clp.is>
2021-02-12 03:18:17 -0500

    styling

--- src/App.svelte
+++ src/App.svelte
@@ -9,16 +9,20 @@
   });
 </script>
 
+<svelte:head>
+  <title>devisa.io</title>
+</svelte:head>
+
 <style>
   h1 {
     font-size: 5.9rem;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     font-weight: 100;
-    text-shadow: 1px 2px 9px rgba(0,0,0,0.4);
-    color: #fff;
+    /* text-shadow: 1px 2px 9px rgba(0,0,0,0.4); */
+    /* color: #fff; */
   }
   :global(body) {
-    background-image: linear-gradient(#343048, #291f32);
+    /* background-image: linear-gradient(#343048, #291f32); */
     max-height: 100%;
     max-width: 100%;
     overflow: auto;
@@ -27,9 +31,13 @@
     display: block;
     position: absolute;
   }
+  #i {
+    background: #291f32;
+    color: #b0fe76;
+  }
 
 </style>
 
 <main>
-  <h1>welcome to devisa</h1>
+  <h1>welcome to <span style="background: #b0fe76; color: #444054">devisa</span></h1>
 </main>
